b835840cb944cfd279ac90fcd4542933b82ee875
Andreas Willi <andiwilli8460@gmail.com>
2018-07-01 18:41:38 +0200

    Add async autocomplete for tv show name

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { remote } from 'electron';
+import Autocomplete from 'react-autocomplete';
 
 export default class App extends Component {
 
@@ -9,29 +10,66 @@ export default class App extends Component {
 
     this.baseUrl = 'http://api.themoviedb.org/3';
     this.apiKey = 'a7493504f75921f0d0f10f96d468d6cd';
-    
+
+    this.get = this.makeRequestCreator();
+
     this.state = {
       results: [],
       query: '',
       files: [],
+      selectedTVShow: null,
     };
   }
 
-  async search(e) {
-    e.preventDefault();
-    if (!this.state.query || this.state.query.length < 3) return;
+  makeRequestCreator() {
+    var call;
+    return function(url) {
+      if (call) {
+        call.cancel();
+      }
+      call = axios.CancelToken.source();
+      return axios.get(url, { cancelToken: call.token });
+    }
+  }
 
-    const response = await axios.get(`${this.baseUrl}/search/tv?api_key=${
-      this.apiKey}&language=de&query=${this.state.query}`);
+  async search(query) {
+    if (!query || query.length < 3) {
+      this.setState({
+        results: [],
+        selectedTVShow: null,
+      });
+      return;
+    }
+
+    try {
+      const response = await this.get(`${this.baseUrl}/search/tv?api_key=${
+        this.apiKey}&language=de&query=${query}`);
+      this.setState({
+        results: response.data.results,
+      });
+    } catch(e) {
+      if (axios.isCancel(e)) return;
+      console.log(e);
+    }
+  }
+
+  handleChange(e) {
     this.setState({
-      results: response.data.results,
+      query: e.target.value,
+    });
+    this.search(e.target.value);
+  }
+
+  handleSelect(query, item) {
+    this.setState({
+      query,
+      selectedTVShow: item.id,
     });
   }
 
   async open() {
     const files = await remote.dialog.showOpenDialog({
       properties: ['openFile', 'multiSelections', 'createDirectory'],
-      message: 'hi',
     });
     if (!files) return;
     this.setState({
@@ -44,25 +82,20 @@ export default class App extends Component {
       <div>
         <h1>Title</h1>
         <button onClick={this.open.bind(this)}>open</button>
-        <form onSubmit={this.search.bind(this)}>
-          <input
-            onChange={(e) => {
-              this.setState({
-                query: e.target.value
-              });
-            }}
-            value={this.state.query} />
-        </form>
-        <br />
-        {this.state.query}
-        <br />
+        <Autocomplete
+          getItemValue={(item) => item.name}
+          items={this.state.results}
+          renderItem={(item, isHighlighted) =>
+            <div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+              {item.name} ({item.first_air_date.substr(0,4)})
+            </div>
+          }
+          value={this.state.query}
+          onChange={this.handleChange.bind(this)}
+          onSelect={this.handleSelect.bind(this)}
+        />
         <ul>
-          {this.state.results.map(
-            r => <li key={r.id}>{r.name} ({r.first_air_date.substr(0,4)})</li>
-          )}
-        </ul>
-        <ul>
-          {this.state.files.map(f => <li key={f}>{f}</li>)}
+        {this.state.files.map(f => <li key={f}>{f}</li>)}
         </ul>
       </div>
     );
