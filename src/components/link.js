3feba18d5cdf54eb7dd21f427a13e467390d3268
Andreas Willi <andiwilli8460@gmail.com>
2018-09-15 16:31:10 +0200

    Introduce link component to open urls in browser

---
+++ src/components/link.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { shell } from 'electron';
+
+export default class Link extends Component {
+  render() {
+    return (
+      <span
+        className="link"
+        onClick={() => shell.openExternal(this.props.url)}
+      >{this.props.label}</span>
+    );
+  }
+}
