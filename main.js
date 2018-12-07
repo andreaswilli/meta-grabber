8dc59400d30fb20af6c36b9d38d9fb156cac649e
Andreas Willi <andiwilli8460@gmail.com>
2018-12-07 03:53:09 +0100

    Add menu bar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu, shell } = require('electron')
+const defaultMenu = require('electron-default-menu')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -27,7 +28,14 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+
+  // menu bar
+  let menu = defaultMenu(app, shell)
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
