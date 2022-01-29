33d48f3c147f9b07a00945797b9301aef5e51d31
Andreas Willi <andiwilli8460@gmail.com>
2022-01-29 19:29:14 +0100

    Add test case and refactor

--- tests/app.test.js
+++ tests/app.test.js
@@ -10,16 +10,45 @@ jest.setTimeout(timeout)
 
 beforeAll(async () => {
   page = await electronTest.createPage()
+  await loadDefaultSettings()
+  await page.reload()
 })
 
 afterAll(async () => electronTest.destroyPage())
 
 describe('Home', () => {
   test('show initial instructions in snackbar', async () => {
-    await page.waitForSelector('.message__text')
-    const message = await page.$eval('.message__text', (el) => el.innerText)
-    expect(message).toBe(
+    await expectText(
+      '.message__text',
       'Please search for a TV show and open the files you want to rename.'
     )
   })
+
+  test('change ui language', async () => {
+    await page.click('.settings-button')
+    await wait(400) // css transition
+    await expectText('h2', 'Settings')
+    await page.click('[data-test-id=button-ui-lang-de]')
+    await expectText('h2', 'Einstellungen')
+    await page.click('[data-test-id=button-ui-lang-ch]')
+    await expectText('h2', 'Istellige')
+    await page.click('[data-test-id=button-ui-lang-en]')
+    await expectText('h2', 'Settings')
+  })
 })
+
+function wait(ms) {
+  return new Promise((res) => setTimeout(res, ms))
+}
+
+async function loadDefaultSettings() {
+  await page.evaluate(() => {
+    localStorage.setItem('uiLang', 'en')
+    // TODO: set rest of localstorge to default values
+  })
+}
+
+async function expectText(selector, expectedText) {
+  const actualText = await page.$eval(selector, (el) => el.innerText)
+  expect(actualText).toBe(expectedText)
+}
